868f92e6c06ef3f5fe4bde409b3b01c90e42dd33
René Pfeuffer <rene@pfeuffer-web.de>
2019-01-31 08:45:51 +0100

    Fix sonar issues

--- src/main/java/sonia/scm/legacy/GravatarMD5Util.java
+++ src/main/java/sonia/scm/legacy/GravatarMD5Util.java
@@ -1,26 +1,35 @@
 package sonia.scm.legacy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.security.MessageDigest;
 
 /**
  * Gravatar MD5 hash util from https://de.gravatar.com/site/implement/images/java/
  */
-public class GravatarMD5Util {
-    public static String hex(byte[] array) {
-        StringBuffer sb = new StringBuffer(array.length * 2);
+final class GravatarMD5Util {
+
+    private static final Logger logger = LoggerFactory.getLogger(GravatarMD5Util.class);
+
+    private GravatarMD5Util() {
+    }
+
+    static String md5Hex (String email) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            return hex (md.digest(email.getBytes("CP1252")));
+        } catch (Exception e) {
+            logger.warn("could not create MD5 hash for email {}", email, e);
+            return null;
+        }
+    }
+
+    private static String hex(byte[] array) {
+        StringBuilder sb = new StringBuilder(array.length * 2);
         for (byte b : array) {
             sb.append(Integer.toHexString((b & 0xFF) | 0x100).substring(1, 3));
         }
         return sb.toString();
-    }
-
-    public static String md5Hex (String email) {
-        try {
-            MessageDigest md =
-                    MessageDigest.getInstance("MD5");
-            return hex (md.digest(email.getBytes("CP1252")));
-        } catch (Exception e) {
-            return null;
-        }
     }
 }
